cbd90294a621b06629342748d37f5ebe1360e865
Joep Sales <info@incomtechdesign.com>
2022-06-02 18:07:04 +0200

    Some change

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ const onListening = () => {
 const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 app.use(cors());
-// Some update
+
 const server = http.createServer(app);
 server.on("error", onError);
 server.on("listening", onListening);
